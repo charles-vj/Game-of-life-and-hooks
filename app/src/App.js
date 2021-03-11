f6a40e642640757280a51b63bd95c14078305f8a
charles-vj <charles2019@iiitkottayam.ac.in>
2021-03-12 03:01:55 +0530

    grid initialised

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import produce from 'immer';
+const numRows=50;
+const numCols=50;
 function App() {
+
+  const[grid,setGrid] = useState(() => {
+    const rows = [];
+    for(let i=0;i<numRows; i++)
+    {
+      rows.push(Array.from(Array(numCols),() => 0))
+    }
+
+    return rows;
+  });
+  console.log(grid);
+
+  const[running, setRunning]=useState(false);
+
   return (
-    <div className="App">
-      hi
+    <>
+    <button onClick={() => setRunning(!running)}>{running ? 'start' : 'stop'}</button>
+    <div style={{
+      display : 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {grid.map((rows,i) => rows.map((col,k) => <div
+       
+       key={`${i}-${k}`}
+
+       onClick={() => {
+        const newGrid = produce(grid, gridCopy => {
+          gridCopy[i][k] = grid[i][k] ? 0 : 1;
+        })
+        setGrid(newGrid);
+       }}
+
+       style={{
+        width : 20 ,
+        height : 20,
+        backgroundColor: grid[i][k] ? 'pink' : undefined,
+        border : "solid 1px black"
+    
+    }}/>))}
     </div>
+  </>
   );
 }
 
